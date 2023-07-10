f15cb864db537f1a5f5a9da9a5f11fcb5c6ee25d
Andrew Wang <45743645+andreww3@users.noreply.github.com>
2023-07-10 14:54:48 +1000

    Create app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import data from './exampletextstimuli.js';
 import './App.css';
 
 function App() {
+
+  const [ text, setText ] = useState(Math.floor(Math.random() * (data.length+1)));
+  const [ prog, setProg ] = useState(0);
+  const max_prog = 7;
+
+  function handleNext() {
+    setProg(prev => prev+1);
+  }
+
+  function handleNextText() {
+    setProg(0);
+    setText(Math.floor(Math.random() * (data.length+1)));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <p>{data[text][prog]}</p>
+
+        {(prog<max_prog) && <button onClick={handleNext}>Next</button>}
+
+        {(prog==max_prog) && <button onClick={handleNextText}>Next text</button>}
+        
       </header>
     </div>
   );
